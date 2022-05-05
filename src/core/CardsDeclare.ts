44171fe0173871660c013691caa98c9d1b8178bd
MYD <2663797538@qq.com>
2022-05-05 22:50:07 +0800

    发牌逻辑

---
+++ src/core/CardsDeclare.ts
@@ -0,0 +1,29 @@
+/**
+ * 花色声明
+ */
+export enum SUIT {
+    SPADE, //黑桃
+    HEART, //红心
+    CLUB,  //梅花
+    DIAMOND //方块
+}
+
+/**
+ * 特殊花色
+ * X = 10, S = 小王, Z = 大王
+ */
+export enum SpecialCardName {
+    A = 1,
+    X = 10,
+    J, Q, K,
+    S = 14, Z = 15
+}
+
+/**
+ * 扑克定义
+ */
+export interface Card {
+    num: number,  //点数
+    suit: string,   //花色
+    isUniversal?: boolean  //是否赖子
+}
--- src/core/CardsManage/DealCards.ts
+++ src/core/CardsManage/DealCards.ts
@@ -1,10 +1,47 @@
+import {Card, SUIT, SpecialCardName} from "../CardsDeclare";
+import {Random} from "../utils/Random";
+
 /**
  * 发牌
  * @param player 玩家人数，默认3人
- * @param num 发牌套数，默认1套
+ * @param cardBundleNum 发牌套数，默认1套
  * @param isShuffle 是否洗牌，默认洗
+ * @param hasUniversal 是否赖子，默认否
  */
-function dealCards(player: number = 3, num: number = 1, isShuffle: boolean = true) {
-  console.log(num, isShuffle)
+function dealCards(player: number = 3, cardBundleNum: number = 1, isShuffle: boolean = true, hasUniversal: boolean = false): Array<Array<Card>> {
+    //TODO 赖子玩法、不洗牌
+    //制牌
+    let cardHeap: Array<Card> = [];   //牌堆
+    for (let i = 0; i < cardBundleNum; i++) {
+        Array.from({length: 13}, (length, i) => i).forEach(v => {
+            [0, 1, 2, 3].forEach(k => {
+                let card: Card = {num: v + 1, suit: SUIT[k]};
+                cardHeap.push(card);
+            })
+        })
+        cardHeap.push({num: 14, suit: SUIT[0]});
+        cardHeap.push({num: 15, suit: SUIT[1]});
+    }
+    //发牌
+    let playerCards: Array<Array<Card>> = new Array<Array<Card>>(player)  //玩家的手牌
+    //WARNING：不能使用Array.fill快速填充空数组，否则会产生相同的引用
+    for (let i = 0; i < playerCards.length; i++) {
+        playerCards[i] = []
+    }
+    //洗牌
+    for (let i = 0; i < 10; i++) {
+        cardHeap.sort(function () {
+            return (0.5 - Math.random());
+        });
+    }
+    //发牌
+    cardHeap.forEach((v, i) => {
+        playerCards[i % player].push(v);
+    })
+
+    //TODO 排序
+    console.log(playerCards)
+    return playerCards;
 }
 
+dealCards()
