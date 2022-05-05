a92e75d3fd0eea9b4b5d0a1fd07e54306d71fae6
flymyd <2663797538@qq.com>
2022-05-05 18:12:09 +0800

    init

---
+++ src/core/CardsManage/DealCards.ts
@@ -0,0 +1,10 @@
+/**
+ * 发牌
+ * @param player 玩家人数，默认3人
+ * @param num 发牌套数，默认1套
+ * @param isShuffle 是否洗牌，默认洗
+ */
+function dealCards(player: number = 3, num: number = 1, isShuffle: boolean = true) {
+  console.log(num, isShuffle)
+}
+
